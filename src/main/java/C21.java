a52efae917649fd300af3c909d8afaaab823de2b
Iván Lage Vázquez <ilagev@gmail.com>
2015-10-01 17:03:35 +0200

    Added C21

---
+++ src/main/java/C21.java
@@ -0,0 +1,16 @@
+
+public class C21 {
+    
+    public C21 () {
+        
+    }
+    
+    public String m1 () {
+        return "m1";
+    }
+    
+    public String m2 () {
+        return "m2";
+    }
+
+}
